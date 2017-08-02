bae2d351cffa5f3bbdefd4b1ee3fdc6a834bccaa
Alby Thomas <albytho@umich.edu>
2017-08-01 23:52:09 -0400

    Add files via upload

--- stock.py
+++ stock.py
@@ -12,6 +12,8 @@ currentDate = 0
 currentStock = 0
 
 def get_data(stock_name):
+	dates[:] = []
+	prices[:] = []
 	url = 'http://www.google.com/finance/historical?q=NASDAQ%3A'+stock_name+'&output=csv%27&output=csv'
 	with closing(requests.get(url, stream=True)) as r:
 		reader = csv.reader(codecs.iterdecode(r.iter_lines(), 'utf-8'))
@@ -34,12 +36,6 @@ def get_data(stock_name):
 
 def predict_prices(dates,prices,x):
 	dates = np.reshape(dates,(len(dates),1))
-
-	svr_lin = SVR(kernel='linear',C=1e3)
-	svr_lin.fit(dates,prices)
-	
-	svr_poly = SVR(kernel='poly',C=1e3, degree = 2)
-	svr_poly.fit(dates,prices)
 	
 	svr_rbf = SVR(kernel='rbf',C=1e3, gamma=0.1)
 	svr_rbf.fit(dates,prices)
@@ -47,7 +43,21 @@ def predict_prices(dates,prices,x):
 
 	return svr_rbf.predict(x)[0]
 
-get_data("TSLA")
-print(dates)
-print(prices)
-print(predict_prices(dates,prices,21))
+stocks = []
+elapsed_prediction_times = []
+while True:
+	stock_name = input()
+
+	if len(stock_name) == 0:
+		break
+	else:
+		time = input()
+		stocks.append(stock_name)
+		elapsed_prediction_times.append(float(time))
+
+
+for index, stock in enumerate(stocks):
+	get_data(stock)
+	print(predict_prices(dates,prices,20+elapsed_prediction_times[index]))
+
+
